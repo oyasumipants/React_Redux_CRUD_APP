b2a760033419410071f1813f9e236dc47efd485c
Keigo Ibaraki <keigo.ibaraki@siiibo.com>
2021-05-14 03:35:52 +0900

    add: Class Component and Functional Component
    クラスコンポーネントとファンクショナルコンポーネントを追加

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React , { Component } from 'react';
 
-class App extends Component {
-  render () {
-    // const greeting = "Hi!";
-    // const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange = {() => {console.log("I am clicked")}}/>
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render () {
+//     // const greeting = "Hi!";
+//     // const dom = <h1 className="foo">{greeting}</h1>;
+//     // return dom;
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange = {() => {console.log("I am clicked")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Hi!</div>
 }
 
 export default App;
